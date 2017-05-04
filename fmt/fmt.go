7fc4de0eed452f1a4a26dd1fbb8726b19b93791b
Asim Aslam <asim@chuhnk.me>
2017-05-04 09:07:43 +0100

    satisfy the interface

--- fmt/fmt.go
+++ fmt/fmt.go
@@ -2,9 +2,15 @@ package fmt
 
 import (
 	"fmt"
+
+	"github.com/go-log/log"
 )
 
 type fmtLogger struct{}
+
+var (
+	_ log.Logger = New()
+)
 
 func (t *fmtLogger) Log(v ...interface{}) {
 	fmt.Print(v...)
